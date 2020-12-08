0f7679b52565ae0b8a4d11a5702deba964953bb9
Adam Figueroa <admiralfiggins@gmail.com>
2020-12-08 13:42:07 -0800

    reworking file structure and finializing store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Components/Header'
+import MainMain from './Main/MainMain'
 
 function App() {
   return (
