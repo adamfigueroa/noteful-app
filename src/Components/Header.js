e1801b817bea874a884e9a600514624a8064ab02
Adam Figueroa <admiralfiggins@gmail.com>
2020-12-08 12:05:43 -0800

    first commit/file structure added/header roughed in

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Add functionality to load mainpage onCLick
+
+const Header = () => {
+    return (
+        <div className="headerBox">
+            <h1 className="headerText">Noteful</h1>
+        </div>
+    )
+}
+
+export default Header
