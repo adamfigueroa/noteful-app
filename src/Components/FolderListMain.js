5f9648cdad4a22c80d93944875c3d25f96bdeeeb
Adam Figueroa <admiralfiggins@gmail.com>
2020-12-15 11:45:00 -0800

    added notes to notelist

--- src/Components/FolderListMain.js
+++ src/Components/FolderListMain.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import './FolderListMain.css'
 
 const FolderListMain = (props) => {
     const folderLinks = props.folders.map((folder) => {
--- src/Components/NoteListMain.js
+++ src/Components/NoteListMain.js
@@ -1,23 +1,31 @@
 import React from 'react'
+import { format } from 'date-fns';
+import { Link } from 'react-router-dom';
 
-const NoteListMain = () => {
+const NoteListMain = (props) => {
+    let notes = props.notes.map((note) => {
+        let date = new Date(note.modified)
+        let dateNormalizer = format(date, 'do LLL yyyy')
+
+        return (
+            <li className="note" key={note.id}>
+                <div>
+                    <Link to={`/note/${note.id}`}>
+                    <h2>{note.name}</h2>
+                    </Link>
+                    <p>Last change: {dateNormalizer}</p>
+                    <button>Delete</button>
+                </div>
+            </li>
+        )
+    })
+
     return (
         <div>
-            <div>
-                <p>Placeholder 1</p>
-            </div>
-            <div>
-                <p>Placeholder 2</p>
-            </div>
-            <div>
-                <p>Placeholder 3</p>
-            </div>
-            <div>
-                <p>Placeholder 4</p>
-            </div>
-            <div>
-                <p>Placeholder 5</p>
-            </div>
+            <ul className="noteList">
+                {notes}
+            </ul>
+            <button>Add Note</button>
         </div>
     )
 }
